05f89fb6fd37c53301b8e501ade2aa5cb4656d00
Camila Fonseca <camilarqf@gmail.com>
2021-12-21 21:40:10 -0300

    tipos enumerados

---
+++ src/main/java/modelo/Prioridade.java
@@ -0,0 +1,27 @@
+package modelo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum Prioridade {
+    BAIXA(0, "BAIXA"),
+    MEDIA(1, "MEDIA"),
+    ALTA(2, "ALTA");
+
+    private Integer codigo;
+    private String descricao;
+
+    public static Prioridade toEnum(Integer codigo){
+        if(codigo == null){
+            return null;
+        }
+        for(Prioridade x: Prioridade.values()){
+            if(codigo.equals(x.getCodigo())){
+                return x;
+            }
+        }
+        throw new IllegalArgumentException("Prioridade inválida");
+    }
+}
---
+++ src/main/java/modelo/Status.java
@@ -0,0 +1,27 @@
+package modelo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum Status {
+    ABERTO(0, "ABERTO"),
+    ANDAMENTO(1, "ANDAMENTO"),
+    ENCERRADO(2, "ENCERRADO");
+
+    private Integer codigo;
+    private String descricao;
+
+    public static Status toEnum(Integer codigo){
+        if(codigo == null){
+            return null;
+        }
+        for(Status x: Status.values()){
+            if(codigo.equals(x.getCodigo())){
+                return x;
+            }
+        }
+        throw new IllegalArgumentException("Status inválido");
+    }
+}
