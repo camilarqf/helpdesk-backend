bd07ad77bf10fb1ce609c91bc44693f23eb8fa15
Camila Fonseca <camilarqf@gmail.com>
2021-12-25 14:38:15 -0300

    adiciona findAll (Tecnicos)

--- src/main/java/com/projeto/helpdesk/controller/TecnicoController.java
+++ src/main/java/com/projeto/helpdesk/controller/TecnicoController.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping(value = "/tecnicos")
 public class TecnicoController {
@@ -22,5 +25,12 @@ public class TecnicoController {
     public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id){
         Tecnico tecnico = tecnicoService.findById(id);
         return new ResponseEntity<>(new TecnicoDTO(tecnico), HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TecnicoDTO>> findAll(){
+        List<Tecnico> tecnicos= tecnicoService.findAll();
+        List<TecnicoDTO> tecnicosDTO = tecnicos.stream().map(obj -> new TecnicoDTO(obj)).collect(Collectors.toList());
+        return new ResponseEntity<>(tecnicosDTO, HttpStatus.OK);
     }
 }
--- src/main/java/com/projeto/helpdesk/service/TecnicoService.java
+++ src/main/java/com/projeto/helpdesk/service/TecnicoService.java
@@ -6,6 +6,7 @@ import com.projeto.helpdesk.repository.TecnicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -17,5 +18,9 @@ public class TecnicoService {
     public Tecnico findById(Integer id){
        Optional<Tecnico> tecnico = tecnicoRepository.findById(id);
        return tecnico.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! ID:" + id));
+    }
+
+    public List<Tecnico> findAll() {
+        return tecnicoRepository.findAll();
     }
 }
